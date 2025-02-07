4ab16806c5a9d67e025a1b5e4c72d139807b7005
Joel Sundarsingh A <joelsundarsingh2005@gmail.com>
2025-02-07 23:42:50 +0530

    Rotate array

--- Arrays/Easy/rotaterr_array.java
+++ Arrays/Easy/rotaterr_array.java
@@ -1,20 +1,20 @@
 class Solution {
-    public void rotate(int[] nums, int k) {
-        int n=nums.length;
-        if(k==0) return;
-        k=k%n;
-        Reverse(nums , 0 , n-k-1);
-        Reverse(nums , n-k , n-1);
-        Reverse(nums , 0 , n-1);
+  public void rotate(int[] nums, int k) {
+      int n=nums.length;
+      if(k==0) return;
+      k=k%n;
+      Reverse(nums , 0 , n-k-1);
+      Reverse(nums , n-k , n-1);
+      Reverse(nums , 0 , n-1);
 
-    }
-    public static void Reverse(int[] arr, int start, int end) {
-    while (start <= end) {
-      int temp = arr[start];
-      arr[start] = arr[end];
-      arr[end] = temp;
-      start++;
-      end--;
-    }
   }
+  public static void Reverse(int[] arr, int start, int end) {
+  while (start <= end) {
+    int temp = arr[start];
+    arr[start] = arr[end];
+    arr[end] = temp;
+    start++;
+    end--;
+  }
+}
 }
